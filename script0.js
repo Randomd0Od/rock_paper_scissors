cbf020026850e6a27b2aab868fa1035bdb44e071
Randomd0Od <112051800+Randomd0Od@users.noreply.github.com>
2023-11-07 06:14:45 -0600

    Display score per round.  Minor changes.
    Deleted useless lines.  Changed 'game ...' to 'round'.  Indented  everything after '//game round counter' for clarity.

--- script0.js
+++ script0.js
@@ -1,65 +1,69 @@
 //
+//setup the variables
 let game  = "";
 let i = 0;
-while (i < 5) {
-  //text += "<br> the Number Is . . ." + i;
+let userScore = 0;
+let compScore = 0;
+
+//game round counter
+while (i < 5) {  
   i++;
-  //computer choooses
-function getComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3)
-  if (randomNumber === 0) {
-    getComputerChoice = 'rock'
-  }
-  if (randomNumber === 1) {
-    getComputerChoice = 'paper'
-  }
-  if (randomNumber === 2) {
-    getComputerChoice = 'scissors'
-  }
-  return (getComputerChoice)
-}
 
-//
-//user chooses
-let user = prompt('Choose ROCK, PAPER, or SCISSORS:');
-let playerChoice = user.toLowerCase();
+//computer choooses
+  function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3)
+    if (randomNumber === 0) {
+      getComputerChoice = 'rock'
+    }
+    if (randomNumber === 1) {
+      getComputerChoice = 'paper'
+    }
+    if (randomNumber === 2) {
+      getComputerChoice = 'scissors'
+    }
+    return (getComputerChoice)
+  }
+  const computerChoice = getComputerChoice();
 
-const computerChoice = getComputerChoice();
-//console.log(`Player: `, playerChoice)
-//console.log(`Computer: `, computerChoice)
+  //user chooses
+  let user = prompt('Choose ROCK, PAPER, or SCISSORS:');
+  let playerChoice = user.toLowerCase();
 
-//determining who won
-function whoResult() {
-  if (computerChoice === playerChoice) {
-    return ("It's a draw");
+  //determining who won the round
+  function whoResult() {
+    if (computerChoice === playerChoice) {
+      return ("It's a draw");
+    } 
+    if (computerChoice === 'rock' && playerChoice === 'paper') {
+      userScore = userScore + 1;
+      return ("You Win");
+    }
+    if (computerChoice === 'rock' && playerChoice === 'scissors') {
+      compScore = compScore + 1;
+      return ("You lose");
+    }
+    if (computerChoice === 'paper' && playerChoice === 'scissors') {
+      userScore = userScore + 1;
+      return ("You Win");
+    }
+    if (computerChoice === 'paper' && playerChoice === 'rock') {
+      compScore = compScore + 1;
+      return ("You lose");
+    }
+    if (computerChoice === 'scissors' && playerChoice === 'rock') {
+      userScore = userScore + 1;
+      return ("You Win");
+    }
+    if (computerChoice === 'scissors' && playerChoice === 'paper') {
+      compScore = compScore + 1;
+      return ("You lose");
+    }
   }
-  if (computerChoice === 'rock' && playerChoice === 'paper') {
-    return ("You Win");
-  }
-  if (computerChoice === 'rock' && playerChoice === 'scissors') {
-    return ("You lose");
-  }
-  if (computerChoice === 'paper' && playerChoice === 'scissors') {
-    return ("You Win");
-  }
-  if (computerChoice === 'paper' && playerChoice === 'rock') {
-    return ("You lose");
-  }
-  if (computerChoice === 'scissors' && playerChoice === 'rock') {
-    return ("You Win");
-  }
-  if (computerChoice === 'scissors' && playerChoice === 'paper') {
-    return ("You lose");
-  }
-}
 
-//visual results
-  console.log(`Game number: `,i);
+  //visual results
+  console.log(`Round: `,i);
   console.log(`  You chose: ${playerChoice}
   Comp chose: ${computerChoice}
-  ${whoResult()}` )
-  //console.log(`You chose: \t\t ${playerChoice}`)
-  //console.log(`Computer chose: \t ${computerChoice}`)
-  //console.log(`${whoResult()}` )
-  
+  ${whoResult()}
+  You: ${userScore}  Computer: ${compScore}`)    
 }
